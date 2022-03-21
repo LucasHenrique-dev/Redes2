784aae849b7eb17f720db6801b4c53a9c3177b86
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2022-03-21 12:50:21 -0300

    Update snifferWindows.py

--- snifferWindows.py
+++ snifferWindows.py
@@ -6,25 +6,35 @@ import os
 host = "192.168.1.68"   # PORTA IPV4 DA INTERNET (REDE SEM FIO)
 
 # create a raw socket and bind it to the public interface
+# VERIFICA O SISTEMA OPERACIONAL. "nt" INDICA O USO SISTEMA WINDOWS
+##########################################################################
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
 else:
     socket_protocol = socket.IPPROTO_ICMP
+##########################################################################
 
 # CRIA UM SOCKET
 ##########################################################################
 soquete = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
 ##########################################################################
 
-# CAPTURA INFOS DE UMA PORTA ESPECÍFICA DE UMA INTERFACE ESPECÍFICA: BIND(INTERFACE, PORTA)
+# CAPTURA INFOS DE UMA DETERMINADA PORTA DE UMA INTERFACE ESCOLHIDA: BIND(INTERFACE, PORTA)
 ##########################################################################
 soquete.bind((host, 0))
 ##########################################################################
 
+# DEFINE AS OPTIONS DO SOCKET
+#   IP_HDRINCL: GARANTE UM IP HEADER
+##########################################################################
 soquete.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+##########################################################################
 
+# ATIVA O MODO PROMISCUOUS
+##########################################################################
 if os.name == "nt":
     soquete.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+##########################################################################
 
 # RECEBIMENTO DOS DADOS
 ##########################################################################
@@ -41,7 +51,7 @@ binario = bin(inteiro)
 print(f"DADOS BINARIO: {binario[2:]}")
 ##########################################################################
 
-# A DESVENDAR (ALGO SOBRE REMOVER O PORMISCUO)
+# DESATIVA O MODO PROMISCUOUS
 ##########################################################################
 if os.name == "nt":
     soquete.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
