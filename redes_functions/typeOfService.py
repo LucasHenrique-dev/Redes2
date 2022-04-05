a355a57a03cac1aca97afe2c063af52358b05576
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2022-04-05 08:47:27 -0300

    TOS update

--- redes_functions/typeOfService.py
+++ redes_functions/typeOfService.py
@@ -1,15 +1,24 @@
 def type_of_service(data):
     precedence = data >> 5
-    delay = data & 0x10
-    throughput = data & 0x08
-    reliability = data & 0x04
-    cost = data & 0x02
-    print(f"    Precedence: {precedence:>2}")   # MUITAS CATEGORIAS? (VER DSCP, SLIDES; MOSTRAR CASO HAJA?)
+
+    delay, throughput, reliability, cost, reserved = calcular_bits(data)
+
+    print(f"    Precedence: {precedence:>2}")  # MUITAS CATEGORIAS? (VER DSCP, SLIDES; MOSTRAR CASO HAJA?)
     print(f"    Delay: {delay:>7}")
     print(f"    Throughput: {throughput:>2}")  # TAXA DE DADOS TRANFERIDOS
     print(f"    Reliability: {reliability}")
     print(f"    Cost: {cost:>8}")
-    #   ADICIONAR O "MUST BE ZERO"?
+    print(f"    Reserved: {reserved:>4}")
+
+
+def calcular_bits(data):
+    delay = bin(data & 0x10)[2:3]
+    throughput = bin(data & 0x08)[2:3]
+    reliability = bin(data & 0x04)[2:3]
+    cost = bin(data & 0x02)[2:3]
+    reserved = bin(data & 0x01)[2:3]
+
+    return delay, throughput, reliability, cost, reserved
 
 
 """
