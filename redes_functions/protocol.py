4596491c3f14556aa3e57beddb2a636cc0230705
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2022-05-04 15:54:53 -0300

    refatoracao de arquivos

--- redes_functions/protocol.py
+++
@@ -1,9 +0,0 @@
-def protocol(data):
-    print(f"Protocol: {data}\n    Protocol Type:", end=" ")
-
-    if data == 6:
-        print("TCP")
-    elif data == 17:
-        print("UDP")
-    elif data == 1:
-        print("ICMP")
